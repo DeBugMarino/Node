dd037f191ec76d8554e86d9d9f67c08b1ed0d779
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-05-19 19:22:24 +0200

    base request

--- planets.js
+++ planets.js
@@ -7,8 +7,35 @@ const port = 3000;
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+app.get("/api/planets", (req, res) => {
   res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === id);
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
+  res.status(201).json({ msg: "il pianeta è creato" });
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === id ? { ...p, name } : p));
+  res.status(200).json({ msg: "i pianeti sono aggiornati" });
+});
+
+app.delete("/api/planet/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== id);
+
+  res.status(200).json({ msg: "il pianeta è stato eliminato " });
 });
 
 app.listen(port, () => {
